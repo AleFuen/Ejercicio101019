d78fd951b86d4e9ce30a7d85ddb455d9757d6dd8
alefu <aleasmiel@gmail.com>
2019-10-12 22:14:07 -0300

    Ejercicio 101019

--- app/src/main/java/com/example/a101019/MainActivity.java
+++ app/src/main/java/com/example/a101019/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.a101019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap Mapa
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Mapa = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng arica = new LatLng(-34, 151);
+        Mapa.addMarker(new MarkerOptions().position(arica).title("Marcador en Arica"));
+        Mapa.moveCamera(CameraUpdateFactory.newLatLng(arica));
     }
 }
