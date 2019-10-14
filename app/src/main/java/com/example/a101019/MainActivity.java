f3917bf1e6efaae805533117b42c894445bac7c1
alefu <aleasmiel@gmail.com>
2019-10-14 17:49:53 -0300

    Ejercicio 101019

--- app/src/main/java/com/example/a101019/MainActivity.java
+++ app/src/main/java/com/example/a101019/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Mapa.moveCamera(CameraUpdateFactory.newLatLng(elsalvador));
         CameraPosition cameraPosition = CameraPosition.builder()
                 .target(elsalvador)
-                .zoom(2)
+                .zoom(20)
                 .build();
 
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
